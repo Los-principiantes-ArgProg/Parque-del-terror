5fb350ff460a40d315a23abf355471a42636f295
SoledadQuiroz <soledadquiroz96@gmail.com>
2021-09-15 22:45:20 -0300

    se agregan test

--- test/test/ItinerarioTest.java
+++ test/test/ItinerarioTest.java
@@ -1,38 +1,35 @@
 package test;
 
-import static org.junit.Assert.*;
+import paquete.*;
+
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
-import paquete.Archivo;
 import paquete.Atraccion;
-import paquete.Itinerario;
-import paquete.PromocionPorcentual;
-import paquete.TipoAtraccion;
-import paquete.Usuario;
 
 public class ItinerarioTest {
 
 	@Test
 	public void quePuedaCrearAtraccionArchivo() {
-		Archivo archivoAtraccion = new Archivo();
-		Atraccion[] atracciones = archivoAtraccion.obtenerAtraccionesDesdeArchivo();
+		
+		Atraccion[] atracciones = Archivo.obtenerAtraccionesDesdeArchivo();
 
 		assertNotNull(atracciones);
 
 	}
 
 	public void quePuedaCrearUsuarioArchivo() {
-		Archivo archivoUsuario = new Archivo();
-		Atraccion[] usuarios = archivoUsuario.obtenerAtraccionesDesdeArchivo();
+		
+		Atraccion[] usuarios = Archivo.obtenerAtraccionesDesdeArchivo();
 
 		assertNotNull(usuarios);
 
 	}
 
 	public void quePuedaCrearPromocionesArchivo() {
-		Archivo archivoPromocion = new Archivo();
-		Atraccion[] promociones = archivoPromocion.obtenerAtraccionesDesdeArchivo();
+		
+		Atraccion[] promociones = Archivo.obtenerAtraccionesDesdeArchivo();
 
 		assertNotNull(promociones);
 	}
